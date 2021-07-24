fd4da1386a9e6aa681e3f196e4472dac40fb2235
Sundev79 <sundev79.sunproject@gmail.com>
2021-07-24 15:14:34 +0200

    Testing

--- .github/workflows/pre-nightly.py
+++ .github/workflows/pre-nightly.py
@@ -38,9 +38,7 @@ def main() -> None:
     xml_root = get_root(file)
     xml_root[3].text += f"-nightly_{get_commit_id()}"
 
-    xml_root[8][0][1].text = xml_root[8][0][1].text.replace(
-        "stable-builds", "nightly-builds"
-    )
+    xml_root[8][0][1].text = xml_root[8][0][1].text.replace("stable-builds", "nightly-builds")
 
     save_pom(file_dest, xml_root)
 
