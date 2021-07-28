236f269518c532d9a4ec6ba2959c0d658a47b331
Sundev79 <sundev79.sunproject@gmail.com>
2021-07-28 19:30:02 +0200

    Updated Module Name

--- src/main/tests/org/nutdevs/modularkit/AnotherModule.java
+++ src/main/tests/org/nutdevs/modularkit/AnotherModule.java
@@ -5,11 +5,11 @@ import org.nutdevs.modularkit.core.ModularModule;
 public class AnotherModule extends ModularModule {
 
     public AnotherModule() throws Exception {
-        super("ModuleLidl", "452457c5", "Sundev79", "1.0.0");
+        super("AnotherModule", "452457c5", "Kawalize", "1.2.4");
     }
 
     @Override
     public void runEvent() {
-        System.out.println("Hello Lidl !");
+        System.out.println("Hello " + this.getModuleName() + " !");
     }
 }
